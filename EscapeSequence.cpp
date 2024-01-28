245c1698636f4374be9132262a09d4eacc2e3561
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-01-28 20:44:33 +0600

    EscapeSequence

---
+++ EscapeSequence.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<< "Fahim\nNurnabi\nKamrul\n";//Every word print new line
+    cout<< "Fahim \bGood \bBoy\n"; // FahimGoodboy no space when use\b
+    cout << " Daffodil \f University\n";//print with sign
+    cout<< " Software \r Engineering\n";// Carriage Return
+    cout<< " Daffodil \t diu\n";//print space with 4 tab
+    cout<< " \0DIU  CSE";// Null
+    cout<< "\'Diu swe\'\n";// Print line with single quote
+    cout<< "\"swe  diu\"\n";
+    cout<< " Fahim\\Good\n";//print with backslash
+    cout<< " What\?\n";//Print with Question mark
+    cout<< "Fahim studey in diu"<<endl <<"SWE";//print with <<endl mens new line
+}
