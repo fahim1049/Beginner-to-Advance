cfb1fcd8ad59864b0bcd7515264eaf5c3f0bf81d
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-01-28 21:24:00 +0600

    keyword, variable & data type

---
+++ variable.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int num1=20,num2=30;
+    double num3=40.3563426;
+    char ch='A';
+    float num5=4.95;
+
+    cout<< "Number one:" <<num1 <<endl <<"Number Two:" <<num2 <<endl;
+    cout<< "Number Three:" <<num3 <<endl <<"Name is:" <<ch;
+    cout<<"\nFloat Number is:" <<num5;
+}
