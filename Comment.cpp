da77402cd0fd2e0957c8ddbea0f8b8cde5195a74
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-01-28 20:54:42 +0600

    Single land Multiple line comment

---
+++ Comment.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //this is comment program
+
+    /*author
+     fahim
+     daffodil International university
+     software engineering department
+     section a
+     batch 37
+     */
+}
