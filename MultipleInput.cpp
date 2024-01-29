aa85a4e60b3155db10b9325c30c27794e8294338
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-01-29 15:10:01 +0600

    Multiple User Input

---
+++ MultipleInput.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+
+    int num1,num2,sum;
+
+    cout<<"Enter your two number :\n";
+    cin>>num1 >> num2;
+
+    sum = num1 + num2;
+
+    cout<<"Your result is : "<<sum;
+    getch();
+}
