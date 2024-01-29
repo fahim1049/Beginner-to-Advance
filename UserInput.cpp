32913f775a1dc76b62b1bce97ef6da81c439b693
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-01-29 15:09:32 +0600

    cin>> UserInput

---
+++ UserInput.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int num;
+
+    cout<<"Enter your number:";
+    cin >> num;
+
+    cout<<"Number is:" <<num;
+    getch();
+}
