5c754a7a5c314546aa2f1c7d3c4b1e853c8d1af0
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-01-30 22:10:52 +0600

    Area of Ellipse

---
+++ Ellips.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+     double a,b,area;
+    cout<<"Enter your Part a:";
+    cin>>a;
+    cout<<"Enter Your partb:";
+    cin>>b;
+
+    area = 3.14*a*b;
+
+    cout<<"Area of Ellips :"<<area;
+    getch();
+}
+
