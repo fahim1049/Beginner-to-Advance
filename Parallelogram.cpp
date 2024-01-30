7101f78622f4f007ae2be6ff6bff7c5e3672a575
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-01-30 22:12:28 +0600

    Area of Parallelogram

---
+++ Parallelogram.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+     double base,hight,area;
+    cout<<"Enter your base:";
+    cin>>base;
+    cout<<"Enter Your vertical Hight:";
+    cin>>hight;
+
+    area =  base * hight;
+
+    cout<<"Area of Parallelogram :"<<area;
+    getch();
+}
+
