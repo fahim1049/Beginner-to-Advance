b302cb6da6f46402c04cd198a7baaafc3bc5f9f7
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-01-30 22:21:28 +0600

    Area of Circle

---
+++ Circle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+     double radius,area;
+    cout<<"Enter your Radious:";
+    cin>>radius;
+
+
+    area =  3.14 * radius * radius;
+
+    cout<<"Area of Circle :"<<area;
+
+    getch();
+}
+
