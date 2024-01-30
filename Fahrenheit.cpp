2e78c6c42de1c51334285d435e344fcf279b6d11
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-01-30 22:54:24 +0600

    Celsius to Fahrenheit

---
+++ Fahrenheit.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    double far,cel;
+
+    cout<<"Enter your Celcious :";
+    cin>>cel;
+
+    far = 1.8 * cel+32;
+
+    cout<<"Fahrenheit = "<<far;
+    getch();
+
+}
