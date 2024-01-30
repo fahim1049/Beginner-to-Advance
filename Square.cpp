77a7448597a1cdbf9a9c59813dd433871d4f054d
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-01-30 22:11:20 +0600

    Area of Square

---
+++ Square.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+     double length,area;
+    cout<<"Enter your Length of side:";
+    cin>>length;
+
+
+    area = length*length;
+
+    cout<<"Area of Square :"<<area;
+    getch();
+}
+
