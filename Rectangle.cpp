cef9e9e17dcf790ee18eafafd66bd539ce85debd
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-01-30 21:34:32 +0600

    Area of Rectangle

---
+++ Rectangle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+     double width,hight,area;
+    cout<<"Enter your width:";
+    cin>>width;
+    cout<<"Enter Your Hight:";
+    cin>>hight;
+
+    area =  width * hight;
+
+    cout<<"Area of Rectangle :"<<area;
+    getch();
+}
