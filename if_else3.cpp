3b6f7da7763e2d0aca05c190147157b08fc859b4
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-01-31 16:43:26 +0600

    Bigger or Smaller

---
+++ if_else3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int num1,num2;
+    cout<<"Enter number One :";
+    cin>>num1;
+    cout<<"Enter Number Two :";
+    cin>>num2;
+
+    if(num1>num2)
+    {
+        cout<<"Number One is Bigger";
+
+    }else{
+        cout<<"Number Two is bigger";
+    }
+    getch();
+}
