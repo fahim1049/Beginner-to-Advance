2dc8f8345a97b93af493e2a144ae12eac13c415b
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-01-31 16:42:14 +0600

    Even or ODD

---
+++ else_if.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter an number :";
+    cin>>num;
+
+    if(num%2==0){
+        cout<<"Even";
+    }
+    else{
+        cout<<"odd";
+    }
+    getch();
+}
