2e46d264b6a4a02feb0db52b8c55bb3d842bde6b
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-01-31 16:44:05 +0600

    High School Letter Grade

---
+++ LetterGrade.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{   int mark;
+    cout<<"Enter Your Number:";
+    cin>>mark;
+
+    if(mark>100 || mark<0)
+    {
+        cout<<"Invalid";
+    }
+    else if(mark >=80 && mark<=100)
+    {
+        cout<<"A+";
+    }
+    else if(mark>=70 && mark<=79)
+    {
+        cout<<"A";
+    }
+    else if(mark>=60 && mark<=69)
+    {
+        cout<<"A-";
+
+    }
+    else if(mark>=50 && mark<=59)
+    {
+        cout<<"B";
+
+    }
+    else if(mark>=40 && mark<=49)
+    {
+        cout<<"C";
+    }
+    else if(mark>=33 && mark<=39)
+    {
+        cout<<"D";
+    }
+    else{
+        cout<<"Fail";
+    }
+    getch();
+}
+
