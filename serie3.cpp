c6d9e278cd8c8be23ec246fc1b0e09a81153bea8
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-02-02 11:55:32 +0600

    sum of odd from 1 to n

---
+++ serie3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int  main()
+{
+    int sum=0,n;
+    cout<<"Enter Your Last number :";
+    cin>>n;
+    for(int i=1;i<=n;i=i+2)
+    {
+       sum =sum + i;
+
+    }
+
+     cout<<"Your sum is :"<<sum;
+
+    getch();
+}
+
