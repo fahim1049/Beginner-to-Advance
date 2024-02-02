821ae52c36c1b5a0785ce0f3d19537eb8b55d48e
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-02-02 12:03:18 +0600

    Print sum of 1 to n number with  power 2

---
+++ series4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int  main()
+{
+    int sum=0,n;
+    cout<<"Enter Your Last number :";
+    cin>>n;
+    for(int i=1;i<=n;i=i+1)
+    {
+       sum =sum + i*i;
+
+    }
+
+     cout<<"Your sum is :"<<sum;
+
+    getch();
+}
+
