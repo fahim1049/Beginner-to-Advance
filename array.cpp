1a2e539ccc538ac383959a81d977d0e2f085c4df
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-02-03 18:09:33 +0600

    test array

---
+++ array.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int marks[4];
+
+    marks[0]=10;
+    marks[1]=20;
+    marks[2]=30;
+
+    cout<<marks[0] <<" ";
+    cout<<marks[1] <<" ";
+    cout<<marks[2] <<" ";
+
+}
+
