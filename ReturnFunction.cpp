bce61ac81c69fc1184c2ac5159162f4c6fdde9a7
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-02-06 18:34:30 +0600

    Return with Function

---
+++ ReturnFunction.cpp
@@ -0,0 +1,19 @@
+# include<iostream>
+#include<conio.h>
+using namespace std;
+
+
+
+int additon(int a,int b)
+{
+   int sum = a+b;
+   return sum;
+
+}
+int main()
+{
+   cout<< additon(10,20);
+   getch();
+
+
+}
